99995b8ce8726a7763c29af81303553af96c376a
Jenise Ruehle <jeniseruehle@gmail.com>
2021-07-28 11:23:46 -0500

    update controllers, item model, + migrations & seeds

--- iDue-api/app/controllers/items_controller.rb
+++ iDue-api/app/controllers/items_controller.rb
@@ -1,2 +1,25 @@
 class ItemsController < ApplicationController
+    def index
+        items = Item.all.order_by_created_at
+        render json: ItemSerializer.new(items)
+    end
+
+    def create
+        item = Item.new(item_params)
+        if item.save
+            render json: ItemSerializer.new(item), status: :accepted
+        end
+    end
+
+    def destroy
+        item = Item.find_by(id: params[:id])
+        item.delete
+        render json: item
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:name, :description, :price, :duedate, :list_id)
+    end
 end
